c4d2755d2cc0816ca92577f2365ecfc27b0f2690
Sedlyarsky Aleksey <Aleksey.Sedlyarsky@infotecs.ru>
2018-02-14 17:39:32 +0300

    Added license briefs

--- agent/activity.py
+++ agent/activity.py
@@ -1,9 +1,27 @@
+'''
+The telestandx is a Telegram bot for optimize team work with hardware stands running by Linux.
+Copyright (C) 2017  Sedlyarskiy Alexey
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'''
+
 ''' It does NOT support Wayland '''
 import multiprocessing
 import os
-
 from Xlib import display
-from os import subprocess
+
 
 class ProcessWatcher:
     '''
@@ -11,32 +29,25 @@ class ProcessWatcher:
         cd perf-tools
         sudo ./execsnoop
     '''
-    def __init__(self):
+    def __init__(self, pipe):
         self.events = list()
+        self.pipe = pipe
         self.proc = None
         self.stop = False
-
-        def install_watcher(path):
-            if not os.path.exists(path):
-
-                os.mknod(path)
-                os.system('cd {} && git clone https://github.com/brendangregg/perf-tools.git'.format(path))
-
-            return os.path.join(path, 'perf-tools/execsnoop')
-
-        self.elf_path = install_watcher('/opt/telestandx_agent_watcher')
 
     def add_event(self, handler):
         self.events.append(handler)
 
     def routine(self):
-        self.proc = subprocess.Popen(self.elf_path, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        self.proc = os.subprocess.Popen('execsnoop', stdout=os.subprocess.PIPE, stderr=os.subprocess.STDOUT)
         while True:
             if self.stop:
                 break
             line = self.proc.stdin.readline()
             for e in self.events:
-                e(line)
+                res = e(line)
+                if res:
+                    self.pipe.send(res)
 
     def stop(self):
         self.stop = True
@@ -47,10 +58,6 @@ class ActivityWatcher:
     def __init__(self):
         self.mouse_x = 0
         self.mouse_y = 0
-        self.console_hashsum = 0
-
-        # def setup_bashrc():
-        #     with open()
 
     def is_mouse_changed(self, display_env=':0'):
         data = display.Display().screen().root.query_pointer()._data
@@ -62,14 +69,6 @@ class ActivityWatcher:
             return True
 
         return True
-
-    def is_console_changed(self):
-        ''' "PROMPT_COMMAND='history -a" must be in ~/.bashrc '''
-        ''' export PROMPT_COMMAND="echo $('date') > ~/.bash_last_activity" '''
-
-
-
-        return False
 
 
 if __name__ == '__main__':
--- server/models/users_queue.py
+++ server/models/users_queue.py
@@ -1,3 +1,22 @@
+'''
+The telestandx is a Telegram bot for optimize team work with hardware stands running by Linux.
+Copyright (C) 2017  Sedlyarskiy Alexey
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'''
+
 import queue
 from collections import deque
 
